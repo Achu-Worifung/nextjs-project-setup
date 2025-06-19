20906377af5b5f5016ac9172f902870ff0922a11
Alan Rivera <alan.rivera@padgettenterprisesllc.com>
2025-06-19 11:46:47 -0700

    navbar

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++ .next/static/chunks/app_layout_tsx_c0237562._.js
@@ -2,7 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/_bc743cfb._.js"
+  "static/chunks/node_modules_12508201._.js",
+  "static/chunks/_7706a81d._.js"
 ],
     source: "dynamic"
 });
---
+++ components/ui/profile-bubble.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+interface ProfileBubbleProps {
+  name: string;
+  avatarUrl?: string;
+  size?: number; // px
+}
+
+const getInitials = (name: string) =>
+  name
+    .split(" ")
+    .map((n) => n[0])
+    .join("")
+    .toUpperCase();
+
+export const ProfileBubble: React.FC<ProfileBubbleProps> = ({
+  name,
+  avatarUrl,
+  size = 110,
+}) => (
+  <div
+    style={{
+      width: size,
+      height: size,
+      borderRadius: "50%",
+      background: "#f3f4f6",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      overflow: "hidden",
+      fontWeight: 600,
+      fontSize: size * 0.9,
+      color: "#374151",
+      border: "2px solid #e5e7eb",
+      userSelect: "none",
+    }}
+    title={name}
+  >
+    {avatarUrl ? (
+      <img
+        src={avatarUrl}
+        alt={name}
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
+      />
+    ) : (
+      getInitials(name)
+    )}
+  </div>
+);
+
+export default ProfileBubble;
