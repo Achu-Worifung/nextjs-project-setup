e2c4088662fbb30d10118cf1ebe272ba3aaf07db
worifung achu <achuclinton47@gmail.com>
2025-06-22 00:20:32 -0700

    removed node module git tracker

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Xj8DgSjrSSX5RXdpEvxU5Mp6s0Rt4q8bb62wpIZHPYI=\"\n}"
--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/node_modules_7fd3e2c6._.js",
-  "static/chunks/_7706a81d._.js"
-],
-    source: "dynamic"
-});
