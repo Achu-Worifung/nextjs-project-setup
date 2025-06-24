6224f4fdff3d8a736e0b8912ef76c82a7f83b3ee
ira puranik <106283965+irapuranik@users.noreply.github.com>
2025-06-24 10:35:44 -0700

    connect to db

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import { ModeToggle } from "@/components/mode-to-toggle";
+import {updateMenuItem} from "@/test-db"
 
 export default function Home() {
+
+  updateMenuItem();
   return (
     <div className="flex items-center justify-between px-8 py-4 bg-gradient-to-r ">
       
