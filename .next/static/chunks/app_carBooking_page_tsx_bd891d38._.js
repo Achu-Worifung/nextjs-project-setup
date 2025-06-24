275a65a0c63f657d450636c7a2b9328cbb50e636
worifung achu <achuclinton47@gmail.com>
2025-06-23 20:35:17 -0700

    Remove .next/ from Git tracking

--- .next/static/chunks/app_carBooking_page_tsx_bd891d38._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_flowbite-datepicker_dist_main_esm_8a1ca310.js",
-  "static/chunks/node_modules_ccc1ffdc._.js",
-  "static/chunks/components_c50ee64f._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_page_tsx_5f881403._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_eb9ed20a._.js",
-  "static/chunks/components_c2043cb8._.js"
-],
-    source: "dynamic"
-});
