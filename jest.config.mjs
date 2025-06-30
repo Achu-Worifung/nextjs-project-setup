a4788e20c0f35322239c916cc99ce8dafc50ea73
Alan Rivera <alan.rivera@padgettenterprisesllc.com>
2025-06-30 10:33:21 -0700

    jest testing added

---
+++ jest.config.mjs
@@ -0,0 +1,39 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  // All imported modules in your tests should be mocked automatically
+  // automock: false,
+  testEnvironment: "jsdom",
+
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: undefined,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+  
+  // Module name mapper to handle path aliases
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+export default config;
